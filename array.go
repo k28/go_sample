2cf0a54e420d472b44e1e03a092a745ee7bf1bbb
Kazuya Hatano <k28@me.com>
2020-03-02 06:20:01 +0900

    update

--- array.go
+++ array.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func array() {
 	// The type [n]T is an array of n values of type T.
 	// An array's length is part of its type, so arrays cannot be resized.
 	var a [2]string
@@ -32,8 +32,62 @@ func main() {
 	fmt.Println(c, d)
 
 	// A slice does not store any data, it just describes a section of an underlying array.
-	// Changeing the elements of a slice modifies the corresponding elements of its underlying array/
+	// Changeing the elements of a slice modifies the corresponding elements of its underlying array.
 	d[0] = "XXX"
 	fmt.Println(c, d)
 	fmt.Println(names)
+}
+
+func arraySlice() {
+	fmt.Println("--------- array slice ---------")
+	s := []int{2, 3, 5, 7, 11, 13}
+	printSlice(s)
+
+	s = s[1:4]
+	printSlice(s)
+
+	s = s[:2]
+	printSlice(s)
+
+	q := s[1:]
+	printSlice(q)
+	printSlice(s)
+}
+
+func sliceAppend() {
+	fmt.Println("--------- slice append ---------")
+
+	var s []int
+	printSlice(s)
+
+	// append works on nil slice
+	s = append(s, 0)
+	printSlice(s)
+	s = append(s, 1)
+	printSlice(s)
+	s = append(s, 2, 3, 4)
+	printSlice(s)
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
+
+func rangeSample() {
+	fmt.Println("--------- range sample ---------")
+	var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
+	// ranging over a slice, two values are returnd for each iteration.
+	// index and value(copy of the element)
+	// can skip the index or value by assigning to _.
+	for i, v := range pow {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
+}
+
+func main() {
+	array()
+	arraySlice()
+	sliceAppend()
+	rangeSample()
 }
